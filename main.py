74c04effb997f7d09689fafeb5ac2b0833d92f6d
JoseGustavo <josegustavo@outlook.com>
2024-05-05 00:36:24 +0200

    progress bar

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import random
 import json
 from lcp.src.problems.problems import Problems
 from lcp.src.algorithm import Population, GeneticAlgorithm
-from lcp.src.graphic.draw_container import draw
-from concurrent.futures import ThreadPoolExecutor
 from lcp.src.algorithm.population import GroupImprovement
 from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm
+import multiprocessing
 
-
-# random.seed(43)
+random.seed(42)
 
 types_count = [5, 10, 15, 20]
 
 for i in types_count:
     Problems(file_path='problems/types_%d.json' % i)\
-        .generate(10, N_TYPES=i, BOX_SIDE_MIN=250, BOX_SIDE_MAX=750)
+        .generate(25, N_TYPES=i, BOX_SIDE_MIN=250, BOX_SIDE_MAX=750)
 
 improvements = [GroupImprovement.none,
                 GroupImprovement.during,
@@ -38,7 +37,7 @@ def solve(args):
 
 
 def main():
-    with ProcessPoolExecutor(max_workers=8) as executor:
+    with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()-1) as executor:
         args = []
         for i in types_count:
             problems = Problems(file_path='problems/types_%d.json' %
@@ -46,14 +45,15 @@ def main():
             for problem in problems:
                 num_types = len(problem.box_types)
                 args += [(problem, imp, num_types) for imp in improvements]
-        print("Cantidad de problemas a resolver: %s" % len(args))
+        # print("Cantidad de problemas a resolver: %s" % len(args))
         results = executor.map(solve, args)
 
-        for result in results:
-            with open('results.txt', 'a') as outfile:
-                json.dump(result, outfile)
-                outfile.write('\n')
-            print(result)
+        with tqdm(total=len(args)) as pbar:
+            for result in results:
+                with open('results.txt', 'a') as outfile:
+                    json.dump(result, outfile)
+                    outfile.write('\n')
+                pbar.update()
 
 
 if __name__ == "__main__":
